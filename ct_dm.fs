00da1a55cb4f27ca968d6ca82739427f0f170e1a
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-24 15:30:45 -0400

    Update ct_dm.fs

--- ct_dm.fs
+++ ct_dm.fs
@@ -1,10 +1,5 @@
 open System
 
-let sign num =
-   if num > 0 then "positive"
-   elif num < 0 then "negative"
-   else "zero"
-
-let main() = Console.WriteLine("sign 5: {0}", (sign 5))
+let main() = Console.WriteLine("Categories may comprise of sets/groups/topologies/etc.")
 
 main()
